125e577bc1215154051fbd17443c6179e62b18cd
Niels Maerten <niels@maerten.be>
2019-03-27 12:04:49 +0100

    chore: Moving projects and cleaning up code

--- packages/nightscout-assistant-www/functions/nightscout.js
+++ packages/nightscout-assistant-www/functions/nightscout.js
@@ -1,35 +1,42 @@
-const fetch = require("node-fetch")
-const admin = require('firebase-admin')
-const r = require("url")
-module.exports = async (userEmail) => {
-
+const fetch = require("node-fetch");
+const admin = require("firebase-admin");
+const r = require("url");
+module.exports = async userEmail => {
   if (admin.apps.length === 0) {
-    admin.initializeApp()
+    admin.initializeApp();
   }
 
   return new Promise(async (resolve, reject) => {
     // TODO: hash email so we don't have to store addresses in our db
-    userEmail = userEmail.replace("@", "").replace(".", "")
+    userEmail = userEmail.replace("@", "").replace(".", "");
 
-    const snapshot = await admin.app()
+    const snapshot = await admin
+      .app()
       .database()
       .ref("users/" + userEmail + "/ns")
       .once("value");
 
-    const nsUrl = snapshot.val().url
+    const nsUrl = snapshot.exists() ? snapshot.val().url : null;
     if (!nsUrl) {
-      resolve("Looks like you haven't linked your Nightscout site yet. " +
-        "To continue, visit https://github.com/nielsmaerten/nightscout-assistant")
+      resolve(
+        "Looks like you haven't linked your Nightscout site yet. " +
+          "To continue, visit https://github.com/nielsmaerten/nightscout-assistant"
+      );
     } else {
-      fetch(nsUrl + '/api/v1/entries/current.json')
+      fetch(nsUrl + "/api/v1/entries/current.json")
         .then(res => res.json())
         .then(json => {
-          resolve({
-            glucose: json[0].sgv,
-            trend: json[0].direction,
-            timestamp: new Date()
-          })
+          resolve(formatResponse(json[0]));
+        })
+        .catch(() => {
+          resolve(
+            "Sorry, I couldn't reach your Nightscout site. Check if you've entered the correct URL on https://github.com/nielsmaerten/nightscout-assistant"
+          );
         });
     }
   });
+};
+
+function formatResponse(d) {
+  return `${d.sgv} and ${d.direction} as of ${8} minutes ago.`;
 }
