7715836851021df640cdbfb317d2887182975f8d
Niels Maerten <niels@maerten.be>
2019-06-27 16:33:19 +0200

    Refactor/health disclaimer (#18)
    As required by Google, Nightscout Status will now say a health disclaimer during the first conversation.

---
+++ functions/debug.js
@@ -0,0 +1,12 @@
+const ns = require("./nightscout");
+const { i18next, initLocale } = require("./i18n");
+
+initLocale().then(() => {
+  ns.getNightscoutStatus(
+    {
+      nsUrl: "https://my-nightscout.herokuapp.com"
+    },
+    "user@example.com",
+    i18next.getFixedT("en")
+  );
+});
--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,104 @@
 // @ts-check
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 const nightscout = require("./nightscout");
 const { dialogflow, SignIn } = require("actions-on-google");
 const { i18next, initLocale } = require("./i18n");
+const config = functions.config().dialogflow || require("./config");
 
+// Set dialogflow client ID
 const app = dialogflow({
-  clientId:
-    "348868123018-imkge00ovotkf89nq48hj0h00aesubuk.apps.googleusercontent.com"
+  clientId: config.clientid
 });
 
+// Initialize localization module
+const initializedLocale = initLocale();
+
+// Ensure Firebase is initialized (only once)
+if (admin.apps.length === 0) {
+  admin.initializeApp();
+}
+
 app.intent("Glucose Status", async conv => {
-  await initLocale(conv.user.locale);
+  // Get t function for this user's locale
+  await initializedLocale;
+  const t = i18next.getFixedT(conv.user.locale);
+
+  // Check: does the user have an active account?
   if (conv.user.profile.token === undefined) {
-    conv.ask(new SignIn(i18next.t("signIn.request")));
+    // NO: Ask them to sign in
+    conv.ask(new SignIn(t("signIn.request")));
   } else {
+    // YES:
+
+    // Get user profile from db
     const userEmail = conv.user.email;
-    const nsResponse = await nightscout(userEmail);
-    conv.close(nsResponse);
+    const userProfile = await nightscout.getUserProfile(userEmail);
+
+    // Get current glucose from Nightscout
+    const nightscoutStatus = await nightscout.getNightscoutStatus(
+      userProfile,
+      userEmail,
+      t
+    );
+
+    // Health Disclaimer: spoken on the first successful query
+    let healthDisclaimer = "";
+    if (nightscoutStatus.success && !userProfile.hasHeardHealthDisclaimer) {
+      healthDisclaimer = t("signIn.healthDisclaimer");
+    }
+
+    // Speak the response and end the conversation
+    conv.close(
+      `<speak>
+    ${nightscoutStatus.response}
+    <break time="500ms"/>
+    ${healthDisclaimer}
+    </speak>`
+    );
+
+    // Update the profile if we said the health disclaimer
+    if (healthDisclaimer !== "") {
+      userProfile.hasHeardHealthDisclaimer = true;
+      await nightscout.updateUserProfile(userProfile, userEmail);
+    }
   }
 });
 
-app.intent("Sign In", async (conv, params, signin) => {
-  await initLocale(conv.user.locale);
-  if (signin.status === "OK") {
-    let completionText = `
-    <speak>
-    ${i18next.t("signIn.healthDisclaimer")}
-    <break time="500ms"/>
-    ${i18next.t("signIn.completed")}
-    </speak>
-    `
-    conv.close(completionText);
-  } else {
+app.intent("Sign In", async (conv, params, signIn) => {
+  // Get t function for this user's locale
+  await initializedLocale;
+  const t = i18next.getFixedT(conv.user.locale);
+
+  // @ts-ignore
+  // Quit if user didn't sign in
+  if (signIn.status !== "OK") {
     conv.close();
+  } else {
+    // ASSISTANT SAYS: "Great, your new account is set up. You'll get a confirmation email soon."
+
+    // Get user's profile form db
+    const userEmail = conv.user.email;
+    const userProfile = await nightscout.getUserProfile(userEmail);
+
+    // Has the user already set up an account?
+    if (!userProfile) {
+      // No user profile yet, prompt user to visit site and set it up
+      // ASSISTANT SAYS: "Before I can get your glucose, you'll need to give me the url to your ns site..."
+      conv.close(t("errors.noNsSite"));
+    } else {
+      // 'Returning' user. Say health disclaimer again, and end conversation
+      // by saying how to invoke it next time.
+      conv.close(
+        `<speak>
+      ${t("signIn.healthDisclaimer")}
+      <break time="500ms"/>
+      ${t("signIn.completed")}
+      </speak>`
+      );
+      userProfile.hasHeardHealthDisclaimer = true;
+      await nightscout.updateUserProfile(userProfile, userEmail);
+    }
   }
 });
 
