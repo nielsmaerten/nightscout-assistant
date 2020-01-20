0c73d1f50188ba6af1b991dba504c0ed57b1d863
Niels Maerten <niels@maerten.be>
2020-01-21 00:43:12 +0100

    create 'locales' package with merge locale files

---
+++ packages/locales/create-symlinks.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const fs = require("fs");
+
+const localePaths = {
+  source: path.resolve("./strings"),
+  cloudFunctions: path.resolve("../cloud-functions/i18n/languages"),
+  webApp: path.resolve("../webapp/public/locales")
+};
+
+fs.symlinkSync(localePaths.source, localePaths.cloudFunctions, "junction");
+fs.symlinkSync(localePaths.source, localePaths.webApp, "junction");
