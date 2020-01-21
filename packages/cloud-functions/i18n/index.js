713c497c65601e0c9296b769e44d7a7d0d8c0102
Niels Maerten <niels@maerten.be>
2020-01-21 01:00:37 +0100

    link all locales

--- packages/cloud-functions/i18n/index.js
+++ packages/cloud-functions/i18n/index.js
@@ -1,5 +1,4 @@
 const i18next = require("i18next").default;
-
 module.exports.i18next = i18next;
 
 module.exports.initLocale = async function() {
@@ -12,17 +11,24 @@ module.exports.initLocale = async function() {
     },
     resources: {
       da: require("./locales/da.json"),
-      fr: require("./locales/fr.json"),
-      pl: require("./locales/pl.json"),
-      pt: require("./locales/pt.json"),
+      de: require("./locales/de.json"),
       en: require("./locales/en.json"),
-      sv: require("./locales/sv.json"),
+      es: require("./locales/es.json"),
+      fr: require("./locales/fr.json"),
+      hi: require("./locales/hi.json"),
+      id: require("./locales/id.json"),
       it: require("./locales/it.json"),
+      ja: require("./locales/ja.json"),
+      ko: require("./locales/ko.json"),
       nl: require("./locales/nl.json"),
       no: require("./locales/no.json"),
-      de: require("./locales/de.json"),
-      ja: require("./locales/ja.json"),
-      es: require("./locales/es.json")
+      pl: require("./locales/pl.json"),
+      pt: require("./locales/pt.json"),
+      ru: require("./locales/ru.json"),
+      sv: require("./locales/sv.json"),
+      th: require("./locales/th.json"),
+      uk: require("./locales/uk.json"),
+      zh: require("./locales/zh.json")
     }
   });
 };
