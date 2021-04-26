31f65160f4835c99857bf5877f46a28d5f14a2d0
Niels Maerten <niels@maerten.be>
2021-04-26 15:56:16 +0200

    Update notification pointing to Gluco Check

--- packages/webapp/src/components/ns-notification.vue
+++ packages/webapp/src/components/ns-notification.vue
@@ -1,15 +1,39 @@
 <template>
-  <div class="notification is-success">
-    &nbsp;
-    <strong>Gluco Check</strong> (aka <strong>Nightscout Status 2.0</strong>)
-    is now available in bèta.
-    <a href="https://glucocheck.app">Give it a try!</a>
-    &nbsp;🎉
+  <div>
+    <div
+      class="notification is-info"
+      v-if="$store.state.languages.active === 'en'"
+    >
+      &nbsp;
+      <strong>Gluco Check</strong> (<strong>Nightscout Status 2.0</strong>) is
+      now available in English.
+      <a href="https://glucocheck.app">Give it a try!</a>
+      &nbsp;🎉
+    </div>
+    <div
+      class="notification is-info"
+      v-if="$store.state.languages.active !== 'en'"
+    >
+      &nbsp;
+      <strong>Gluco Check (Nightscout Status 2.0)</strong>
+      is now available in English. You can
+      <a href="https://glucocheck.app">try it out here</a>, or help us
+      <a href="https://translate.glucocheck.app">
+        translate to {{ currentLanguage }} </a
+      >.
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "ns-notification"
+  name: "ns-notification",
+  computed: {
+    currentLanguage() {
+      return this.$store.state.languages.available.find(v => {
+        return v.code === this.$store.state.languages.active;
+      }).name;
+    }
+  }
 };
 </script>
